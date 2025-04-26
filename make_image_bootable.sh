563ed1e9e6ca775ed54b85730340578895a559d3
Krystian Włodek <79439853+krycha1248@users.noreply.github.com>
2025-04-26 16:20:23 +0200

    Update make_image_bootable.sh

--- make_image_bootable.sh
+++ make_image_bootable.sh
@@ -6,15 +6,24 @@ DISK="/dev/sda"
 
 echo "[*] Reinstalling GRUB on ${DISK}..."
 
+# Instalacja kernela z RPM
 dnf install -y /root/kernel-core*.rpm
 
-# Reinstall GRUB
+# Odświeżenie initramfs (dracut)
+echo "[*] Regenerating initramfs..."
+for KERNEL_VERSION in $(rpm -q kernel-core --qf '%{VERSION}-%{RELEASE}.%{ARCH}\n'); do
+    dracut --force --kver "${KERNEL_VERSION}"
+done
+
+# Reinstall GRUB na dysku
+echo "[*] Reinstalling GRUB..."
 grub2-install "${DISK}"
 
-# Regenerate GRUB configuration
+# Regeneracja konfiguracji GRUB-a
+echo "[*] Regenerating GRUB config..."
 grub2-mkconfig -o /boot/grub2/grub.cfg
 
-# Synchronize disk
+# Synchronizacja danych na dysk
 sync
 
 echo "[+] GRUB reinstalled and configured successfully."
